4682f6623614cb8759c56d0cf8b1b6301171c9f8
ahatovanm <ahatovanm@rbtechnologies.ru>
2020-04-14 15:45:25 +0300

    create vk callback bot, add get current weather

---
+++ flask_app.py
@@ -0,0 +1,111 @@
+from flask import Flask, globals
+import vk
+import json
+import pyowm
+from utils import get_translating_press_value, get_weather_description_by_code, get_wind_description
+from settings import *
+
+
+app = Flask(__name__)
+owm = pyowm.OWM(OWM_ACCESS_TOKEN)
+# owm.is_API_online()
+
+
+GET_WEATHER_BUTTON, HOURLY_FORECAST_BUTTON, CHOOSE_DAY_BUTTON = range(3)
+
+BUTTONS = {
+    GET_WEATHER_BUTTON: {
+        "action": {
+            "type": "text",
+            "payload": {"button": GET_WEATHER_BUTTON},
+            "label": 'Получить прогноз погоды'
+        },
+        "color": "primary"
+    },
+    HOURLY_FORECAST_BUTTON: {
+        "action": {
+            "type": "text",
+            "payload": {"button": HOURLY_FORECAST_BUTTON},
+            "label": "Почасовой прогноз погоды"
+        },
+        "color": "primary"
+    },
+    CHOOSE_DAY_BUTTON: {
+        "action": {
+            "type": "text",
+            "payload": {"button": CHOOSE_DAY_BUTTON},
+            "label": "Выбрать другой день"
+        },
+        "color": "primary"
+    }
+}
+
+
+def send_response(user_id, msg_text):
+    session = vk.Session()
+    api = vk.API(session, v=5.50)
+    api.messages.send(
+        access_token=VK_API_ACCESS_TOKEN,
+        user_id=str(user_id),
+        message=msg_text,
+        keyboard=json.dumps({
+            "one_time": False,
+            "buttons": [
+                [{
+                    'action': {
+                        'type': 'text',
+                        "payload": "{\"button\": \"1\"}",
+                        "label": 'Получить прогноз погоды'
+                    },
+                    "color": "primary"
+                }]
+            ]
+        })
+    )
+
+
+def get_weather():
+    observation = owm.weather_at_place('Minsk,BY')
+    w = observation.get_weather()
+    return 'Сейчас в Минске {cur_temperature}°C, '\
+           '{weather_description}, '\
+           'ветер {wind_speed} м/с {wind_description}, '\
+           'влажность {humidity_value}%, '\
+           'давление {press_value} мм рт. ст.'.format(
+                cur_temperature=int(w.get_temperature('celsius')['temp']),
+                weather_description=get_weather_description_by_code(w.get_weather_code()),
+                wind_speed=w.get_wind()['speed'],
+                wind_description=get_wind_description(w.get_wind()['deg']),
+                humidity_value=w.get_humidity(),
+                press_value=get_translating_press_value(w.get_pressure()['press'])
+           )
+
+
+@app.route('/', methods=['POST'])
+def hello_world():
+    data = globals.request.get_json()
+    print(data)
+    assert 'type' in data
+    assert data.get('group_id') == GROUP_ID
+    if data.get('type') == 'confirmation':
+        return CALLBACK_API_CONFIRMATION_TOKEN
+    elif data['type'] == 'message_new':
+        if data.get('object', {}).get('out') == 1:
+            print('this is my msg')
+            return 'ok'
+        if data.get('object', {}).get('body', '').strip() == "Начать":
+            send_response(
+                user_id=data['object']['user_id'],
+                msg_text='Привет! Это умный сервис прогноза погоды. Сделан в рамках задания "Школа будущих СТО" Яндекс.Облака.'
+            )
+            return 'ok'
+        if data.get('object', {}).get('body', '').strip() == "Получить прогноз погоды":
+            send_response(
+                user_id=data['object']['user_id'],
+                msg_text=get_weather()
+            )
+        return 'ok'
+
+
+if __name__ == '__main__':
+    app.run()
---
+++ utils.py
@@ -0,0 +1,47 @@
+from decimal import Decimal, ROUND_HALF_UP
+import os
+import json
+
+
+WINDS = [
+    (0, 'северный'),
+    (45, 'северо-восточный'),
+    (90, 'восточный'),
+    (135, 'юго-восточный'),
+    (180, 'южный'),
+    (225, 'юго-западный'),
+    (270, 'западный'),
+    (315, 'северо-западный')
+]
+
+
+HECTOPASCALS_TO_MERCURY_MILLIMETERS = Decimal(1.333224)
+
+
+FILE_PATH_OF_WEATHER_DESCRIPTION = os.path.join(
+    os.path.abspath(
+        os.path.dirname(__file__)
+    ),
+    'translation_weather_status.json'
+)
+
+
+def get_wind_description(wind_degree: int) -> str:
+    assert 0 <= wind_degree < 360, 'Not valid value of given wind degree: expected between 0 and 360.'
+    for u in range(1, len(WINDS) - 1):
+        if (WINDS[u-1][0] + WINDS[u][0]) / 2 <= wind_degree < (WINDS[u][0] + WINDS[u+1][0]) / 2:
+            return WINDS[u][1]
+    return WINDS[0][1]
+
+
+def get_translating_press_value(press_value: int) -> int:
+    return int((Decimal(press_value) / HECTOPASCALS_TO_MERCURY_MILLIMETERS).quantize(1, rounding=ROUND_HALF_UP))
+
+
+def get_weather_description_by_code(weather_code: int) -> str:
+    with open(FILE_PATH_OF_WEATHER_DESCRIPTION, 'r') as f:
+        weather_status = json.load(f)
+    suitable_descriptions = list(filter(lambda el: el['id'] == weather_code, weather_status))
+    assert len(suitable_descriptions) > 0, 'Not found weather description with given weather_code.'
+    assert suitable_descriptions[0].get("ru"), 'Not valid format of weather description for given weather_code.'
+    return suitable_descriptions[0]["ru"]
